02ac7d2873a0321ed94da841e21fd7afca0dd9e1
Yvone Huynh <sw.yvone.huynh@gmail.com>
2018-01-23 16:35:58 -0500

    added additional progress bars

--- dev/scripts/breathe.js
+++ dev/scripts/breathe.js
@@ -26,7 +26,51 @@ export default class Breathe extends React.Component {
                     <div id="myBar"></div>
                 </div>
                     <button onClick={this.progress}>Hello</button>
-                
+                <div>
+
+                    <div className="breathe-box-container">
+                        <div className="directions">
+                            <div className="message">Inhale</div>
+                            <div className="message">Hold</div>
+                            <div className="message">Exhale</div>
+                            <div className="message">Hold</div>
+                        </div>
+                        <div className="timer inhale">
+                            <div className="progress"></div>
+                            <div className="indicators">
+                                <div className="indicator"></div>
+                                <div className="indicator"></div>
+                                <div className="indicator"></div>
+                                <div className="indicator"></div>
+                            </div>
+                        </div>
+                        <div className="timer inhale-hold">
+                            <div className="indicators">
+                                <div className="indicator"></div>
+                                <div className="indicator"></div>
+                                <div className="indicator"></div>
+                                <div className="indicator"></div>
+                            </div>
+                        </div>
+                        <div className="timer exhale">
+                            <div className="indicators">
+                                <div className="indicator"></div>
+                                <div className="indicator"></div>
+                                <div className="indicator"></div>
+                                <div className="indicator"></div>
+                            </div>
+                        </div>
+                        <div className="timer exhale-hold">
+                            <div className="indicators">
+                                <div className="indicator"></div>
+                                <div className="indicator"></div>
+                                <div className="indicator"></div>
+                                <div className="indicator"></div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         )
     }
