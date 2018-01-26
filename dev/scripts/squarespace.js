d07823880b8272f1d01e917a33bb928b1a9f83f8
Yvone Huynh <sw.yvone.huynh@gmail.com>
2018-01-26 14:41:05 -0500

    added refresh button

--- dev/scripts/squarespace.js
+++ dev/scripts/squarespace.js
@@ -5,17 +5,21 @@ import Homepage from "./homepage-button";
 export default class Squarespace extends React.Component {
     constructor(){
         super();
-        this.changeColour = this.changeColour.bind(this)
+        this.changeColour = this.changeColour.bind(this);
+        this.resetPage = this.resetPage.bind(this)
     }
     changeColour(e){
         e.target.style.backgroundColor = "white";
         e.target.innerHTML = "breathe";
     }
+    resetPage(){
+        document.location.reload(true)
+    }
     render(){
         return (
             <div className="squarespace-container">
             <Header />
-                <p>Click on each white square to turn it red.</p>
+                <p>Click on each white square to turn it white.</p>
                 <div className="mini-square-container">
                     <div className="mini-square" onClick={this.changeColour}></div>
                     <div className="mini-square" onClick={this.changeColour}></div>
@@ -39,6 +43,7 @@ export default class Squarespace extends React.Component {
                     <div className="mini-square" onClick={this.changeColour}></div>
                 </div>
                 <Homepage />
+                <button onClick={this.resetPage}>Rest</button>
             </div>
         )
     }
