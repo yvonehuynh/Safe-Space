bdd58a758cfa91e68d8b6e9ecb399ce609fd250b
Yvone Huynh <sw.yvone.huynh@gmail.com>
2018-01-26 23:03:50 -0500

    added semi-final styles

--- dev/scripts/accomplishments.js
+++ dev/scripts/accomplishments.js
@@ -1,25 +1,58 @@
 import React from "react";
 import Homepage from "./homepage-button";
+import Header from "./header";
 
 export default class Accomplishments extends React.Component {
+    constructor(){
+        super();
+        this.state={
+            accomplishments: ""
+        }
+        this.congrats = this.congrats.bind(this)
+    }
+    congrats(){
+        this.accomplishments.value= ""
+    }
     render(){
         return (
             <div className="accomplishment-container">
-            <form>
-                <p>Name 3 things you accomplished today</p>
-                <ol>
-                        <li><input type="text" autocomplete="off"/></li>
-                        <li><input type="text" autocomplete="off"/></li>
-                        <li><input type="text" autocomplete="off"/></li>
-                </ol>
-                <p>Name 3 things you want to accomplish tomorrow</p>
-                <ol>
-                        <li><input type="text" autocomplete="off"/></li>
-                        <li><input type="text" autocomplete="off"/></li>
-                        <li><input type="text" autocomplete="off"/></li>
-                </ol>
-                <input type="submit"/>
-                </form>
+                <Header />
+                <div className="accomplishment-flex-container">
+                    <div className="form-container">
+                        <form>
+                            <p>Name 3 things you accomplished today</p>
+                            <ol>
+                                <li><input type="text" name="accomplishments" ref={ref => this.accomplishments = ref}/></li>
+                                <li><input type="text" name="accomplishments" ref={ref => this.accomplishments = ref}/></li>
+                                <li><input type="text" name="accomplishments" ref={ref => this.accomplishments = ref}/></li>
+                            </ol>
+                            <p>Name 3 things you want to accomplish tomorrow</p>
+                            <ol>
+                                <li><input type="text" name="accomplishments" ref={ref=> this.accomplishments = ref}/></li>
+                                <li><input type="text" name="accomplishments" ref={ref => this.accomplishments = ref}/></li>
+                                <li><input type="text" name="accomplishments" ref={ref => this.accomplishments = ref}/></li>
+                            </ol>
+                        </form>
+                        <button onClick={this.congrats}>I am proud of myself!</button>
+                    </div>
+
+                    <div className="accomplishment-description">
+                        <h3>Accomplishments</h3>
+                        <p>Accomplishments lets you remind yourself that everyday, there are small joys that you can celebrate. You might have had a hard day, a bad week, month or year but that doesn't mean that you shouldn't acknowledge your successes, big or small.</p>
+                        <h3>Examples:</h3>
+                        <ol>
+                            <li>
+                                <p>You ate breakfast when you normally skip breakfast</p>
+                            </li>
+                            <li>
+                                <p>You replaced the toilet paper roll AND put it on the toilet paper roll holder</p>
+                            </li>
+                            <li>
+                                <p>You made that one stranger smile</p>
+                            </li>
+                        </ol>
+                    </div>
+                </div>
                 <Homepage />
             </div>
         )
--- dev/scripts/affirmation.js
+++ dev/scripts/affirmation.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Homepage from "./homepage-button";
+import Header from "./header";
 
 export default class Affirmation extends React.Component {
     constructor(){
@@ -22,15 +23,17 @@ export default class Affirmation extends React.Component {
     render(){
         return (
             <div className="affirmation-container">
-                <div className="affirmation-button">
-                    <button onClick={this.getWords}>Make Me Feel Better</button>
-                </div>
+                <Header />
+                <p>Get a random quote of the day. Maybe, you'll feel inspired enough to creatively express yourself.</p>
                 <div className="affirmation-words">
                     {this.state.quotes.map(res=>{
                         return (
                             <p key={res.id}>"{res.quote}" - {res.author}</p>
-                        )
-                    })}
+                            )
+                        })}
+                </div>
+                <div className="affirmation-button">
+                    <button onClick={this.getWords}>Make Me Feel Better</button>
                 </div>
                 <Homepage />
             </div>
